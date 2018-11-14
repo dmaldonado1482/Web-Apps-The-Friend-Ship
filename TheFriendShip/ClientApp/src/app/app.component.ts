e49f3523e8c4a3286cd3a77c78e0cdebe8a5f9a2
dmaldonado1482@gmail.com <dmaldonado1482@gmail.com>
2018-11-14 17:40:17 -0500

    adding materials

--- TheFriendShip/ClientApp/src/app/app.component.ts
+++ TheFriendShip/ClientApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@NgModule({
+    imports: [BrowserAnimationsModule]
+})
+
 export class AppComponent {
   title = 'Angular App';
 }
