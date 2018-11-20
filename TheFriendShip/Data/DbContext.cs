9f0fb27f7d658b3788505e8b9208cb64a3b001d9
dmaldonado1482@gmail.com <dmaldonado1482@gmail.com>
2018-11-19 23:25:06 -0500

    Added Users model and DbContext

---
+++ TheFriendShip/Data/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheFriendShip.Models;
+
+namespace TheFriendShip.Data
+{
+    public class DbContext : IdentityDbContext<User>
+    {
+        public DbContext (DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- TheFriendShip/Startup.cs
+++ TheFriendShip/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TheFriendShip.Models;
 
 namespace TheFriendShip
 {
@@ -19,6 +22,10 @@ namespace TheFriendShip
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
+
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<DbContext>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
