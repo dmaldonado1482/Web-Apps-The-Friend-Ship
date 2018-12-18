b549f3ce1e693ffab06ba89351e6ff88e0f48da5
dmaldonado1482@gmail.com <dmaldonado1482@gmail.com>
2018-12-18 16:06:21 -0500

    fixes

---
+++ TheFriendShip/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheFriendShip.Data;
+using TheFriendShip.Models;
+
+namespace TheFriendShip.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _users;
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IUserRepository users, IConfiguration configuration)
+        {
+            _users = users;
+            _configuration = configuration;
+        }
+
+        [HttpGet("getusers")]
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _users.GetUsers();
+            return users;
+        }
+
+        [HttpGet("getuser/{id}")]
+        public async Task<User> GetUser(string id)
+        {
+            var user = await _users.GetUser(id);
+            return user;
+        }
+    }
+}
